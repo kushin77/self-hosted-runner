4ecfdc5608d112a8913990536ba73ebe73053744
Akushnir <akushnir@dev-elevatediq-2>
2026-03-05 04:49:55 +0000

    feat(p4): add azure_scale_set module scaffold, example, and CI plan workflow

---
+++ terraform/modules/azure_scale_set/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features = {}
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
+  name                = "runner-vmss"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  sku = var.vm_sku
+  instances = var.capacity
+
+  admin_username = var.admin_username
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.admin_ssh_public_key
+  }
+
+  source_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  rolling_upgrade_policy {
+    max_batch_instance_percent = 20
+  }
+
+  upgrade_mode = "Manual"
+
+  tags = {
+    module = "azure_scale_set"
+  }
+}
