e4bd5d0ab6e703cfcb67b44422b6a766eb661ad2
Automation <automation@github.com>
2026-03-09 01:11:18 +0000

    chore(keda): make prometheus adapter optional and guard chart repo

--- terraform/modules/keda/outputs.tf
+++ terraform/modules/keda/outputs.tf
@@ -3,5 +3,5 @@ output "keda_release_name" {
 }
 
 output "prometheus_adapter_release_name" {
-  value = helm_release.prometheus_adapter.name
+  value = length(helm_release.prometheus_adapter) > 0 ? helm_release.prometheus_adapter[0].name : ""
 }
