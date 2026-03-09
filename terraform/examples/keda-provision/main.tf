07b806e8d5803d173517e4aad718e9160571fed5
Automation <automation@github.com>
2026-03-09 01:42:05 +0000

    examples(keda): configure kubernetes/helm providers to use kubeconfig path (kind-friendly)

--- terraform/examples/keda-provision/main.tf
+++ terraform/examples/keda-provision/main.tf
@@ -1,6 +1,19 @@
-provider "kubernetes" {}
+variable "kubeconfig_path" {
+  type    = string
+  default = "~/.kube/config"
+}
+
+provider "kubernetes" {
+  config_path = pathexpand(var.kubeconfig_path)
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = pathexpand(var.kubeconfig_path)
+  }
+}
 
 module "keda" {
-  source = "../../modules/keda"
+  source    = "../../modules/keda"
   namespace = "keda"
 }
