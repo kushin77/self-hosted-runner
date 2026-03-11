5bbba931dd1293a159da8650fb0934f45604feb2
Automation <automation@nexusshield.local>
2026-03-11 14:04:50 +0000

    observability: fix metric interval, add fallback logging; add GH issue runbook

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,3 +1,18 @@
+output "kms_key_ring_id" {
+  value = google_kms_key_ring.mirror.id
+}
+
+output "kms_crypto_key_id" {
+  value = google_kms_crypto_key.mirror_key.id
+}
+
+output "wif_pool_id" {
+  value = google_iam_workload_identity_pool.secrets_pool.workload_identity_pool_id
+}
+
+output "wif_provider_id" {
+  value = google_iam_workload_identity_pool_provider.secrets_provider.provider_id
+}
 /**
  * Terraform Outputs
  * Exported values for reference or cross-stack dependencies
