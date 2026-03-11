55998c3bab3e62c707b939c9bd57048fde230442
Automation <automation@nexusshield.local>
2026-03-11 00:41:40 +0000

    chore(secrets): redact example tokens and require secret manager (GSM/Vault/KMS)

--- .git-rewrite/t/terraform/modules/azure_scale_set/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vmss_id" {
-  description = "The ID of the VM Scale Set"
-  value       = azurerm_linux_virtual_machine_scale_set.vmss.id
-}
-
-output "vmss_name" {
-  description = "The name of the VM Scale Set"
-  value       = azurerm_linux_virtual_machine_scale_set.vmss.name
-}
